8f1400bee3a54592b9fdd28cb515b7b91aa9915c
fdavialano <89667476+fdavialano@users.noreply.github.com>
2022-10-13 20:42:24 -0300

    Add files via upload

---
+++ autentica/JWT/AutenticaApplication.java
@@ -0,0 +1,13 @@
+package autentica.JWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutenticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutenticaApplication.class, args);
+	}
+
+}
